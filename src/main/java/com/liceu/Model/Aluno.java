2d909492b6fe069351e06dfe3b943ac58ec3513e
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-08-27 15:19:07 -0300

    limpando warnings

--- src/main/java/com/liceu/Model/Aluno.java
+++ src/main/java/com/liceu/Model/Aluno.java
@@ -1,14 +1,9 @@
 package com.liceu.Model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
