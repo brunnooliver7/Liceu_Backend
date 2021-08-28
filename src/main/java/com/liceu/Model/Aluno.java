ab19fbbd8da432a996efd71ab748737876ddbbb1
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-08-28 17:28:09 -0300

    implementando JsonProperty, JsonRootName e XmlRootElement

--- src/main/java/com/liceu/Model/Aluno.java
+++ src/main/java/com/liceu/Model/Aluno.java
@@ -5,12 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity @Table(name = "aluno")
+@JsonRootName("aluno") @XmlRootElement(name = "aluno")
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Aluno {
     
@@ -18,6 +23,7 @@ public class Aluno {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("nomeAluno")
     private String nome;
 
     private int matricula;
--- src/main/java/com/liceu/Model/Professor.java
+++ src/main/java/com/liceu/Model/Professor.java
@@ -5,12 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity @Table(name = "professor")
+@JsonRootName("professor") @XmlRootElement(name = "professor")
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Professor {
     
@@ -18,6 +23,7 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @JsonProperty("nomeProfessor")
     private String nome;
     
     private String materia;
